7c5c2a2c3adc91eec457c98df4c7ae81d9c9c765
brunosenacsp <103936493+brunosenacsp@users.noreply.github.com>
2022-04-19 08:54:19 -0300

    Adição de comando de saída
    Inclusão de "Oi GitHub".

--- 01-sintaxe.php
+++ 01-sintaxe.php
@@ -18,7 +18,8 @@
 
 <?php
     echo "Amanhã tem corinthians e palmeiras!";
-    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.<h2>"
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.<h2>";
+    echo "Oi GitHub";
     
 ?>
 </body>
